7f92af074d866522f40ed217175be40b764831b6
alenmiani <j29zhnm96x@privaterelay.appleid.com>
2026-07-26 11:02:17 +0200

    v1.0.6: maknut donji tab, gumbi ispod ULAZ/OTPIS kartica

--- app.js
+++ app.js
@@ -52,7 +52,7 @@
 
   // --------------- STORAGE KEY ---------------
   const STORAGE_KEY = 'stand-tracker-data';
-  const VERSION = '1.0.5';
+  const VERSION = '1.0.6';
 
   // --------------- STATE ---------------
   let data = null;
