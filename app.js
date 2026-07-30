a1309deb7d0521da651b9766b8960314a4081361
alenmiani <j29zhnm96x@privaterelay.appleid.com>
2026-07-30 12:36:20 +0200

    v1.0.28: ULAZ/OTPIS subtitle duplo veci

--- app.js
+++ app.js
@@ -57,7 +57,7 @@
 
   // --------------- STORAGE KEY ---------------
   const STORAGE_KEY = 'stand-tracker-data';
-  const VERSION = '1.0.27';
+  const VERSION = '1.0.28';
 
   // --------------- STATE ---------------
   let data = null;
