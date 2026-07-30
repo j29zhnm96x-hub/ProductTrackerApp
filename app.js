48430f93a911924c64f3d00ca2b4c95b7af75201
alenmiani <j29zhnm96x@privaterelay.appleid.com>
2026-07-30 12:41:32 +0200

    v1.0.29: suptilni zeleni/crveni background tint za ULAZ/OTPIS

--- app.js
+++ app.js
@@ -57,7 +57,7 @@
 
   // --------------- STORAGE KEY ---------------
   const STORAGE_KEY = 'stand-tracker-data';
-  const VERSION = '1.0.28';
+  const VERSION = '1.0.29';
 
   // --------------- STATE ---------------
   let data = null;
