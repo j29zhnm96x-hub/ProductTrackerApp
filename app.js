4bd266a0c2b2ddc9ca92b7e87cac845096216eb3
alenmiani <j29zhnm96x@privaterelay.appleid.com>
2026-07-30 13:11:13 +0200

    v1.0.30: sifre proizvoda u boji teme - zelene za ULAZ, crvene za OTPIS

--- app.js
+++ app.js
@@ -57,7 +57,7 @@
 
   // --------------- STORAGE KEY ---------------
   const STORAGE_KEY = 'stand-tracker-data';
-  const VERSION = '1.0.29';
+  const VERSION = '1.0.30';
 
   // --------------- STATE ---------------
   let data = null;
